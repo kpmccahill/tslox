d5111cea400656ee3114c1a2be8c3ca64effa1f2
Kyle McCahill <kylemccahill@gmail.com>
2024-01-11 16:13:46 -0600

    initial setup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 import * as fs from 'node:fs';
 
-export function main() {
-    console.log("Hello, World!");
+export function main(args: Array<String>) {
+    if (args.length > 1) {
+        console.log("Usage: npm run tslox [script]");
+        process.exit(64);
+    } else if (args.length == 1) {
+        console.log("This will run the specified script file");
+    } else {
+        console.log("This will launch an interactive session.");
+    }
 }
+
+main(process.argv.slice(2));
