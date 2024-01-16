e32c9aa1a51fe414136c7204b3323cb686956284
Kyle McCahill <kylemccahill@gmail.com>
2024-01-15 20:14:11 -0600

    started ast generation

--- src/expr.example.ts
+++ src/expr.example.ts
@@ -1,7 +1,11 @@
+/** 
+ * This is an example for comparison agains the test AST generation file.
+*/
 import { Token } from "./token";
 
 /**
  * Expr base class defs, all expression classes inherit from this.
+ * This is an example file for what it will look like.
  */
 export abstract class Expr { }
 
